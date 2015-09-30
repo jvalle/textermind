dfb91fbdbf95f006e8b89fca49f974fe3b51db30
jvalle <jcvalle@mail.usf.edu>
2015-09-29 20:30:59 -0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var gulp = require('gulp'),
+    source = require('vinyl-source-stream');
+
+var files = {
+    html   : ['./index.html'],
+    js     : ['./src/game.js', './src/**/*'],
+};
+
+gulp.task('html', function () {
+    gulp.src(files.html)
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('lint', function () {
+    var jshint = require('gulp-jshint'),
+        stylish = require('jshint-stylish');
+
+    gulp.src(files.js[1])
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('js', function () {
+    var browserify = require('browserify'),
+    	es6ify = require('es6ify'),
+        uglify = require('gulp-uglify'),
+        buffer = require('vinyl-buffer');
+
+    var bundler = browserify({
+        entries: files.js[0]
+    });
+
+    var bundle = function() {
+        return bundler
+        	.add(es6ify.runtime)
+        	.transform(es6ify)
+            .bundle()
+            .pipe(source(files.js[0]))
+            .pipe(buffer())
+            // .pipe(uglify())
+            .pipe(gulp.dest('./dist/'));
+    };
+
+    return bundle();
+});
+
+gulp.task('compile', ['html']);
+
+gulp.task('default', ['compile', 'js']);
