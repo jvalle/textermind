6ac8d68ff687d2c582faf3c2286df7aeafd09133
Jason Valle <jcvalle@mail.usf.edu>
2015-10-12 18:47:56 -0700

    logo bitches

--- src/GameStrings.js
+++ src/GameStrings.js
@@ -1,3 +1,3 @@
 export default {
-	newGame: 'You awaken in a dark room.  Your leg is chained to a wall.  Attached to the chains are two wires, one leading to what appears to be a large car battery, and the other leading off to a panel with four (4) empty circles.  Next to the panel, you spot a stack of multi-colored circular objects.'
+	newGame: 'You awaken in a dark room.  Your leg is chained to a wall.  Attached to the chains are two wires, one leading to what appears to be a large car battery, and the other leading off to a panel with four (4) empty circles.  Next to the panel, you spot a stack of multi-colored circular objects.  What would you like to do?'
 };
